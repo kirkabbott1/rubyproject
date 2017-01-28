967cbdc5b195be0f6667f112481467715fbeb500
Kirk Abbott <kirkabbott1@gmail.com>
2017-01-28 17:47:13 -0500

    route of app home changed

--- scrumtious/config/routes.rb
+++ scrumtious/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  # deleted below because making pages#home now root
+  # get 'pages/home'
+
+  root 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
