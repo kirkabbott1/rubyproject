1fafd10f2582f263000084ea851f354cf9095bd9
Kirk Abbott <kirkabbott1@gmail.com>
2017-01-28 17:55:56 -0500

    adds about page

--- scrumtious/config/routes.rb
+++ scrumtious/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  # deleted below because making pages#home now root
+  # commented below because making pages(controller)#home(route) now root
   # get 'pages/home'
 
   root 'pages#home'
 
+  get 'about' => 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
