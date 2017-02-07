2f7ae9158c39e7ad998c3d17d883778d6e8e391e
Kirk Abbott <kirkabbott1@gmail.com>
2017-02-07 15:48:16 -0500

    adds and updates actions, creates edit view file

--- scrumtious/app/controllers/tasks_controller.rb
+++ scrumtious/app/controllers/tasks_controller.rb
@@ -3,7 +3,9 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
-
+  def edit
+    @task = Task.find(params[:id])
+  end
   def create
     @task = Task.new(tasks_params)
     if @task.save
@@ -11,6 +13,16 @@ class TasksController < ApplicationController
         redirect_to task_path(@task)
     else
       render 'new'
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(tasks_params)
+      flash[:notice] = "Task succesfully updated"
+      redirect_to task_path(@task)
+    else
+      render 'edit'
     end
   end
 
