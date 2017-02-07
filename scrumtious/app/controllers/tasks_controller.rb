8ab741475fd8b9af68a6a6c2fbab141cfb3250ed
Kirk Abbott <kirkabbott1@gmail.com>
2017-02-07 15:15:53 -0500

    adds flash messages, err msgs, show action

--- scrumtious/app/controllers/tasks_controller.rb
+++ scrumtious/app/controllers/tasks_controller.rb
@@ -6,11 +6,17 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(tasks_params)
-    @task.save
-
-    redirect_to task_path(@task)
+    if @task.save
+        flash[:notice] = "task succesfully created"
+        redirect_to task_path(@task)
+    else
+      render 'new'
+    end
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
   private
 
   def tasks_params
