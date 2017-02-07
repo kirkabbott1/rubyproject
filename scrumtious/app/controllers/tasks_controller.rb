3a14c7bf8e0a915b4febdeae4598c4596ead53ef
Kirk Abbott <kirkabbott1@gmail.com>
2017-02-07 16:40:11 -0500

    creates user-task ownership and updates controller

--- scrumtious/app/controllers/tasks_controller.rb
+++ scrumtious/app/controllers/tasks_controller.rb
@@ -1,7 +1,8 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_task, only:[:edit, :update, :show, :destroy]
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   def new
@@ -12,7 +13,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(tasks_params)
+    @task = current_user.tasks.new(tasks_params)
     if @task.save
         flash[:notice] = "task succesfully created"
         redirect_to task_path(@task)
