f811f17f6e4800800a4362f29ce9d3d576d4e22f
Kirk Abbott <kirkabbott1@gmail.com>
2017-02-07 16:24:02 -0500

    adds destroy action, form partial, refactor redundancy

--- scrumtious/app/controllers/tasks_controller.rb
+++ scrumtious/app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-
+  before_action :set_task, only:[:edit, :update, :show, :destroy]
   def index
     @tasks = Task.all
   end
@@ -9,7 +9,6 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def create
@@ -23,7 +22,6 @@ class TasksController < ApplicationController
   end
 
   def update
-    @task = Task.find(params[:id])
     if @task.update(tasks_params)
       flash[:notice] = "Task succesfully updated"
       redirect_to task_path(@task)
@@ -33,10 +31,18 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task.destroy
+    flash[:notice] = "Task succesfully deleted"
+    redirect_to tasks_path
   end
   private
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
   def tasks_params
     params.require(:task).permit(:content)
   end
