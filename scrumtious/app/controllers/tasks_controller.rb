9f245f214684cdfef6c80af1ac8ffacb90c73147
Kirk Abbott <kirkabbott1@gmail.com>
2017-02-07 16:06:20 -0500

    adds show, edit, and index links

--- scrumtious/app/controllers/tasks_controller.rb
+++ scrumtious/app/controllers/tasks_controller.rb
@@ -1,11 +1,17 @@
 class TasksController < ApplicationController
 
+  def index
+    @tasks = Task.all
+  end
+
   def new
     @task = Task.new
   end
+
   def edit
     @task = Task.find(params[:id])
   end
+
   def create
     @task = Task.new(tasks_params)
     if @task.save
